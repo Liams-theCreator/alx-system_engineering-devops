4393ad6041388e0e941a7c5b4c35547bd85f7ed8
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-23 21:14:33 +0000

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Puppet Manifest to  install flask from pip3.
+# Installs puppet-lint.
 
-package { 'flask':
-  ensure   => '2.1.0',   # Specify the desired version of Flask.
-  provider => 'pip3',    # Use pip3 as the package provider.
+file { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
